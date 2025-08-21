0aff85cd15a1e8606740fa31e4a53d79acb5b38e
Devansh-bit <devanshg308@gmail.com>
2025-08-21 14:04:30 +0530

    fix: transfer shenanigans

--- metaploy/postinstall.sh
+++ metaploy/postinstall.sh
@@ -1,23 +1,14 @@
 #!/bin/sh
 
-# Run this script with the binary or the command that starts the project
-# Eg: ./postinstall.sh npm start
-# Eg: ./postinstall.sh ./build/server
-
-# This function removes the metaploy config file from the volume when the project crashes or stops
 cleanup() {
-    echo "Container stopped. Removing nginx HTTPS configuration."
-    rm /etc/nginx/https-enabled/cfmn.metaploy.conf
+	echo "Container stopped. Removing nginx configuration."
+	rm /etc/nginx/sites-enabled/cfmn.metaploy.conf
 }
 
-# Run the cleanup function if either of the below signals are received
 trap 'cleanup' SIGQUIT SIGTERM SIGHUP
 
-# Run the command provided to the arguments (starts the project server)
 "${@}" &
 
-# Copies the metaploy config file to the correct directory
-cp ./cfmn.metaploy.conf /etc/nginx/https-enabled
+cp ./cfmn.metaploy.conf /etc/nginx/sites-enabled
 
-# Waits for the server process to stop (lets it run)
 wait $!
